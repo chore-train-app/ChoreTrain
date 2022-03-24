9cdec572fd5ef22647db882829d1cff7ca7a4ae6
Joanna Christian <joanna.s.christian@gmail.com>
2022-03-24 14:52:31 -0700

    updated chat box

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,24 @@
 $(document).foundation()
+
+//comment box
+let main = function() {
+    $('.btn').click(function() {
+      let post = $('.status-box').val();
+      $('<li>').text(post).prependTo('.posts');
+      $('.status-box').val('');
+    });
+    // $('.status-box').keyup(function() {
+    //   let postLength = $(this).val().length;
+    //   let charactersLeft = 500 - postLength;
+    //   $('.counter').text(charactersLeft);
+    //   if (charactersLeft < 0) {
+    //     $('.btn').addClass('disabled');
+    //   } else if (charactersLeft === 500) {
+    //     $('.btn').addClass('disabled');
+    //   } else {
+    //     $('.btn').removeClass('disabled');
+    //   }
+    // });
+  }
+
+  $(document).ready(main)
