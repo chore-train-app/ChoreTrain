4b8fb990adf35309c43367e23901517b1305799d
Joanna Christian <joanna.s.christian@gmail.com>
2022-03-25 19:33:59 -0700

    initial commit

---
+++ public/js/task.js
@@ -0,0 +1,4 @@
+//event listener for volunteering for a task - trigger taskTaker & Status = true
+// event listener for deleting a task
+// event listener for updating  a task
+// event listener for cancelling a task
